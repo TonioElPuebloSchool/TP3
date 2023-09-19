6ec5646bc51829ed0b960c129afbe747c0487b12
Antoine COURBI <48368458+tonioelpueblo@users.noreply.github.com>
2023-09-19 16:02:24 +0200

    create all files for dev

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    prefixe = """<!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PVE1J5NDNL"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PVE1J5NDNL');
+    </script>"""
+    return prefixe + "Hello from Space! 🚀"
