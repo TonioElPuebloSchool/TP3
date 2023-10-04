e6465167e38440f27dd5c02425b28e8cd31fb4a2
Antoine COURBI <48368458+tonioelpueblo@users.noreply.github.com>
2023-10-04 14:47:40 +0200

    main logger test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -15,3 +16,21 @@ def root():
     </script>"""
     return prefixe + "Hello from Space! 🚀"
 # change
+
+@app.route('/logger', methods=['GET', 'POST'])
+def logger():
+
+    if request.method == 'POST':
+        log_message = request.form['log_message']
+        
+        # Print the log message on the server-side (Python)
+        app.logger.warning(f"Log message from Python: {log_message}")
+
+        # Create JavaScript code to log a message on the browser's console
+        log_browser = f'<script>console.log("Log message from browser: {log_message}");</script>'
+        
+        return log_browser
+    
+    
+    # Render a simple HTML form to input the log message
+    return "Voici un beau log !!"
